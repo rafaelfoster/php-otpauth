900c3c305e1d340182ebae1147b8034561da25c1
Rafael Foster <rfoster@rodrimar.com.br>
2014-12-29 17:52:21 -0200

    Some tests on genkey.php

--- genkey.php
+++ genkey.php
@@ -5,12 +5,12 @@
 	<script src="lib/js/jquery.qrcode-0.11.0.min.js"></script>
 	<script type="text/javascript">
 		$(function(){
+			var strSecret = document.getElementById("qrcode-txt")
+			strSecret = strSecret.innerHTML 
 			$("#qrcode").qrcode({
-			    "render": "div",
-				"width": 100,
-				"height": 100,
+			    "size":   200,
 				"color": "#3a3",
-				"text": "http://larsjung.de/qrcode"
+				"text": strSecret
 			});
 		});
 	</script>
@@ -38,6 +38,6 @@
 
 	print "Get a new Secret: $secret " . "<br>";
 	print "The QR Code for this secret (to scan with the Google Authenticator App: <br><br>";
-	print $g->getURL('chregu','example.org',$secret) . "<br>";
+	print "<a id='qrcode-txt'>" . $g->getURL('chregu','example.org',$secret) . "</a><br>";
 	print "<div id='qrcode'> </div>";
 	print "<br>";
